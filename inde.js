0d781359914edba5181bb7ad089fdb6ecdef74c7
meetprolanceit <meetproexlancer@gmail.com>
2024-12-09 16:54:40 +0530

    add helloword inside the index.js

--- inde.js
+++ inde.js
@@ -1,3 +1,9 @@
-console.log("hii, i am form devloper branch");
-console.log("hii i am coming for the make one branch")
-console.log("meetbhayani")
+console.log('hii, i am form devloper branch');
+console.log('hii i am coming for the make one branch');
+console.log('meetbhayani');
+<<<<<<< Updated upstream
+=======
+const hellWorld = () => {
+    console.log('hello World');
+};
+>>>>>>> Stashed changes
